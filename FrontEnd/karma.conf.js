b6857ca5741575212d6059f3ca1028ba83acd145
Richard Collier <ergosoftuk@gmail.com>
2018-01-18 11:48:24 +0000

    Run without sandbox

--- FrontEnd/karma.conf.js
+++ FrontEnd/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox','--disable-setuid-sandbox','--remote-debugging-port=9222']
       }
     },
     singleRun: true

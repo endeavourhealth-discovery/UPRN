396c8f1221ed4acc1c5801083f2b4c506f215544
Patrick <patrickcrowley@blueyonder.co.uk>
2018-04-16 09:03:57 +0100

    Further checking of address input.

--- API/src/main/java/org/endeavourhealth/dataassurance/utils/UPRNUtils.java
+++ API/src/main/java/org/endeavourhealth/dataassurance/utils/UPRNUtils.java
@@ -8,7 +8,7 @@ public class UPRNUtils {
             return "";
         } else {
             return (data.trim().toLowerCase()
-                    .replace(" ", " ").replace(".", " "));
+                    .replace(" ", " ").replace(".", " ")).replace(" rd", " road");
         }
     }
 
